ce11ac765e27166be91212b1581fc057e4378821
sheriifMahadi <sheriffabdulamjid@gmail.com>
2023-05-14 10:46:50 +0100

    create and style the chat page

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+    title: "Chat"
+}
+
+export default function Chat() {
+    
+    return (
+      <div className="chat-main">
+        <div>
+            <h2>
+                You have no chats &#x3a;&#40;
+            </h2>
+
+            <p>
+            
+                Conversations will appear here if you message another user.  
+            </p>
+        </div>
+
+      </div>
+    )
+}
