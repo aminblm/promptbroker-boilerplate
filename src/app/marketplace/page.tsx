451ce83f9881073f045fef3f13f09b79fcaa2b33
James A. Boulouma <james@boulouma.com>
2023-05-19 06:59:52 +0200

    Updated marketplace

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -5,6 +5,8 @@ import PromptCard from "@/components/PromptCard"
 import { mockPrompts } from "@/mock/data"
 import { iPrompt } from "@/types"
 import { useEffect, useState } from "react"
+import Sidebar from "@/components/home/sidebar" 
+import CardProduct from "@/components/home/cardProduct"
 
 // export const metadata = {
 //   title: "Marketplace"
@@ -20,16 +22,30 @@ export default function Marketplace() {
   })
   return (
     <div className="marketplace">
-      <div className="filters">
+      
+      <div className="flex gap-[2rem]">
+          <div className="filters">
+            <Sidebar></Sidebar>
+            </div>
+        <div className="w-full">
+        <h1 className="text-[1.6rem] font-[500] border-b-2 py-[0.5rem]">Top Prompts of the Past Month</h1>
 
+          <div className="grid grid-cols-4 gap-4 mt-[2rem]">
+            {
+              prompts.map((prompt, idx) => (
+                <PromptCard key={idx} prompt={prompt} />
+                ))
+              }
+            <CardProduct />
+               <CardProduct />
+               <CardProduct />
+               <CardProduct />
+               <CardProduct />
+               <CardProduct />
+          </div>
+              </div>
+          </div>
       </div>
-      <div className="grid">
-        {
-          prompts.map((prompt, idx) => (
-            <PromptCard key={idx} prompt={prompt} />
-          ))
-        }
-      </div>
-    </div>
+      
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import Sidebar from "@/components/home/sidebar" 
-import CardProduct from "@/components/home/cardProduct"
-
-
 export default function Home() {
   return (
-    <div className="flex gap-[2rem]">
-      <Sidebar></Sidebar>
-      <div className="w-full">
-        <h1 className="text-[1.6rem] font-[500] border-b-2 py-[0.5rem]">Top Prompts of the Past Month</h1>
-        <div className="grid grid-cols-4 gap-4 mt-[2rem]">
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-        </div>
-      </div>
+    <div className="">
+      home
     </div>
   )
 }
