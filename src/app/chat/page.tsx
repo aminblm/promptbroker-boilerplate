da4c7012801a8c27f7fbf6cf60b25445bcdbf6a6
sheriifMahadi <sheriffabdulamjid@gmail.com>
2023-05-19 19:10:45 +0100

    fixed errors

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,4 +1,5 @@
 import chats_data from "../../../chat_data"
+import ChatsList from "../../components/ChatsList"
 
 export const metadata = {
     title: "Chat"
@@ -29,41 +30,3 @@ export default function Chat() {
         </div>
     )
 }
-
-export function ChatsList () {
-    return (
-        <>
-        <div className="wrapper">
-           <div className="sidebar">
-               <div className="sidebar-border-bottom">
-                    {chats_data.map(chat_data =>
-                        <div className="conversation-outer-wrapper" key={chat_data.id}>
-                            <div className="conversation-inner-wrapper">
-                                <div className="profile-picture-wrapper">
-                                    <img src={chat_data.image} alt="profile-picture" /> 
-                                </div>
-                                <div className="conversation-info-wrapper">
-                                    <div className="top">
-                                        <div className="chat-user">
-                                            {chat_data.user}
-                                        </div>
-                                        <div className="timestamp">
-                                            {chat_data.timestamp}
-                                        </div>
-                                    </div>
-                                    <div className="bottom">
-                                        <div className="last-message">
-                                            {chat_data.last_message}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        )}
-                </div>
-            </div>
-        </div>
-        </>
-    )
-}
-
