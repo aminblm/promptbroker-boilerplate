efff517a06cf0e6d90fde135b4d1bb1c42345884
sheriifMahadi <sheriffabdulamjid@gmail.com>
2023-05-21 19:54:22 +0100

    message page more styling

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -6,7 +6,7 @@ export default function MessagePane(){
         <div className="message-pane-main">
             <div className="chat-header">
                 <div className="chat-header-left">
-                    <img className="chat-header-back-arrow" src="https://promptbase.com/assets/icons/arrow_back_black_24dp.svg" alt="arrow back" />
+                    <img className="chat-header-back-arrow" src="arrow_back_black_24dp.svg" alt="arrow back" />
                     <div className="chat-header-profile">
                         <img src={chats[1].image} alt="profile" />
                         <div className="chat-header-username">
@@ -14,21 +14,25 @@ export default function MessagePane(){
                         </div>
                     </div>
                 </div>
-                <img className="chat-header-info" src="https://promptbase.com/assets/icons/info.svg" alt="" />
+                <img className="chat-header-info" src="info.svg" alt="chat info" />
             </div>
             <div className="messages-main-wrapper">
                 <div className="message-content-wrapper">
                     <div className="message-content">
-                        <div className="message"></div>
-                        <div className="message-bubble">
-                            <span className="message-text">
-                                Hello World
-                            </span>
-                            <span className="message-timestap-wrap">
-                                    <span className="message-timestamp">
-                                        11:12pm
+                        <div className="message">
+                            <div className="block"></div>
+                            <div className="block">
+                                <div className="message-bubble">
+                                    <span className="message-text">
+                                        Hello World
                                     </span>
-                                </span>
+                                    <span className="message-timestap-wrap">
+                                            <span className="message-timestamp">
+                                                11:12pm
+                                            </span>
+                                        </span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -63,13 +67,12 @@ export default function MessagePane(){
                     <div className="message-icons">
                         <div className="icon">
                             <label>
-                                <img src="https://promptbase.com/assets/icons/attach_image.svg" 
-                                alt="" />
-                                <input _ngcontent-serverapp-c176="" type="file" multiple></input>
+                                <img src="attach_image.svg" alt="attach" />
+                                <input type="file" multiple></input>
                             </label>
                         </div>
                         <div className="icon">
-                                <img src="	https://promptbase.com/assets/icons/send.svg" alt="" />
+                                <img src="send.svg" alt="send" />
                         </div>
                     </div>
                 </div>
