2c9cadbacae8a6d21f817f93c73a6b6a8ef01a79
Festus <festusasiyanbi80@gmail.com>
2023-05-22 23:32:53 +0100

    worked on the FAQ and sell process

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,12 +1,9 @@
-
 "use client"
 
 import PromptCard from "@/components/PromptCard"
 import { mockPrompts } from "@/mock/data"
 import { iPrompt } from "@/types"
 import { useEffect, useState } from "react"
-import Sidebar from "@/components/home/sidebar" 
-import CardProduct from "@/components/home/cardProduct"
 
 // export const metadata = {
 //   title: "Marketplace"
@@ -22,30 +19,15 @@ export default function Marketplace() {
   })
   return (
     <div className="marketplace">
-      
-      <div className="flex gap-[2rem]">
-          <div className="filters">
-            <Sidebar></Sidebar>
-            </div>
-        <div className="w-full">
-        <h1 className="text-[1.6rem] font-[500] border-b-2 py-[0.5rem]">Top Prompts of the Past Month</h1>
-
-          <div className="grid grid-cols-4 gap-4 mt-[2rem]">
-            {
-              prompts.map((prompt, idx) => (
-                <PromptCard key={idx} prompt={prompt} />
-                ))
-              }
-            <CardProduct />
-               <CardProduct />
-               <CardProduct />
-               <CardProduct />
-               <CardProduct />
-               <CardProduct />
-          </div>
-              </div>
-          </div>
+      <div className="filters">
       </div>
-      
+      <div className="grid">
+        {
+          prompts.map((prompt, idx) => (
+            <PromptCard key={idx} prompt={prompt} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,36 @@ import Link from "next/link";
 export default function Footer() {
 
   return (
-    <footer>
+    <footer className="mt-[150px]">
       <p className="copyright">
-        <span>©</span>
-        <span>PromptBroker</span>
+        <span> &copy; PromptBroker</span>
         <span>{new Date().getFullYear()}</span>
       </p>
+
       <div className="otherpages">
-        <Link href=""></Link>
+        <Link href="/faq" className="mr-5 text-sm hover:underline">FAQ </Link>
+        <Link href="/contact" className="mr-5 text-sm hover:underline">Contact </Link>
+        <Link href="/changelog" className="mr-5 text-sm hover:underline">Changelog </Link>
+        <Link href="/blog" className="mr-5 text-sm hover:underline">Blog </Link>
+        <Link href="/privacy" className="mr-5 text-sm hover:underline">Privacy </Link>
+        <Link href="/terms" className="text-sm hover:underline">Terms </Link>
+      </div>
+
+      <div className="social flex my-5">
+      <a href="https://linktr.ee/promptbroker/" className="mr-4"><img src="/icons8-website-50.png" width={35} height={35}></img></a>
+        <a href="https://www.linkedin.com/company/promptbroker/" className="mr-4"><img src="/icons8-linkedin-50.png" width={35} height={35}></img></a>
+        <a href="https://twitter.com/PromptBroker?s=20/"><img src="/icons8-twitter-50.png" width={35} height={35}></img></a>
+      </div>
+
+      <div className="otherpages flex flex-col text-center">
+        <Link href="/" className="text-md hover:underline">Best AI Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best Midjourney V5 Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best Midjourney Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best ChatGPT Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best DALL-E Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best Diffusion Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best GPT-4 Prompts </Link>
+        <Link href="/" className="text-md hover:underline">Best GPT-3 Prompts </Link>
       </div>
     </footer>
   )
