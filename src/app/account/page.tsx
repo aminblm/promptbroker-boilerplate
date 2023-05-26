0d9037d2e7fcbd710659d7f3f77abd9bba97c41b
Benedicta <benedictauche.onyebuchi@gmail.com>
2023-05-26 15:34:24 +0100

    'modified'

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -3,15 +3,14 @@ export const metadata = {
   title: "Favorites",
 };
 
-// import { useRouter } from "next/navigation";
+
 import { usePathname } from "next/navigation";
 import Settings from "./settings/page";
 import FavoritesPage from "./favorites/page";
-// import { Router } from "next/router";
+
 
 export default function Favorites() {
-  // const router = useRouter();
-  // const { route } = router
+
   const pathName = usePathname();
 
   let content;
