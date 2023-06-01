2854a93a29185d48748a140032b68eb7fa32bc07
cartwright <amaechiisaac450@gmail.com>
2023-06-01 23:16:06 +0100

    Created the profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,307 @@
+import React from 'react';
+export default function Profile() {
+  return (
+    <div className="body_container">
+        <div className="hero-container">
+            <div className="hero-banner">
+                <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2Fic5ASefHHEUQNeovrEKj0NqK5343%2Fresized%2F1680856103226_1000x1000.webp?alt=media&token=9266c516-3b78-4c4e-aeb7-97d4844342ee" alt="" />
+            </div>
+            <div className="hero-profile">
+                <div className="profile-image">
+                    <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2Fic5ASefHHEUQNeovrEKj0NqK5343%2Fresized%2F1680855838510_200x200.webp?alt=media&token=29379ebf-839f-4b1c-8371-e6fd0fdfd568" alt="" />
+                </div>
+                <div className="profile-buttons">
+                    <a href="">Message</a>
+                    <a href="">Follow</a>
+                </div>
+            </div>
+            <div className="profile-name">
+                <h3>@Promptbroker</h3>
+            </div>
+        </div>
+        <div className="profile-description">
+            <p>
+            🏆 NEW BONUS: When you purchase this prompt, you will get a second ChatGPT Prompt Generator Prompt from us 🏆 The ONLY AI Prompts Broker you will ever need. 
+            🦊 Best Prompts for Content Creators and Writers. 
+            📚 Prompt Broker has everything you need to take your ideas to the next level. 
+            🚀 DM & Get in touch with us to get you started 🔥
+            </p>
+            <span>
+                <p>1.6k</p>
+                <p>259</p>
+                <p>29</p>
+            </span>
+            <p>Promptbroker Rank: <h5> #387</h5></p>
+            <p>Joined: <h5> Apr 7, 2023</h5></p>
+            <span>
+                <h6>🦊 GPT</h6>
+                <h6>🦊 GPT</h6>
+                <h6>🦊 GPT</h6>
+            </span>
+            <span>
+                <p><h5>1807 </h5> Following</p>
+                <p><h5>66 </h5> Followers</p>
+            </span>
+        </div>
+        <div className="featured-prompt">
+        <div className="featuredprompt-text">
+        <h1>Most Popular Prompts by @promptbroker</h1>
+        </div>
+        <div className="prompts-container">
+        <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FhLJYsvpo5aI1WYQ3vZoV%2Fresized%2F1683909890868_500x500.webp?alt=media&token=a01a99db-6c8b-4202-b9f6-8e968b448783" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Minimal And ...</h6>
+            </div>
+        </div>
+        <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FrsymMlwMbDh3MN3JT5i6dpuMiaj1%2Fresized%2F1683888157123_500x500.webp?alt=media&token=fe10c94f-4823-4a8d-9922-29d1a9d3298c" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Grunge Move...</h6>
+            </div>
+      </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FhPNZYbLWhXGVyPtB1NZ0%2Fresized%2F1683887787122_500x500.webp?alt=media&token=f5953c9c-1942-4e7e-8f6b-87b1b6170567" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Mesmerizing ...</h6>
+            </div>
+          </div> 
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FrZHZRYckkfU3lYMhm1wJrrvtyLD2%2Fresized%2F1683831475270_500x500.webp?alt=media&token=a82f8ea8-ba98-4241-8e35-f485d0dbbc22" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Abstract Acryl...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FAQiQ52YkpsRMq9I0qcZuil3gW1z1%2Fresized%2F1684186600407_500x500.webp?alt=media&token=089af98f-ec34-4737-9a50-d7980238691e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Perfect Vintag...</h6>
+            </div>
+          </div>          
+        </div>
+        <div className="featured-prompt">
+        <div className="featuredprompt-text">
+        <h1>Newest Prompts by @promptbroker</h1>
+        </div>
+        <div className="prompts-container">
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FVAXItKojEQXmXUs4prJNIftWE6T2%2Fresized%2F1684174330042_500x500.webp?alt=media&token=83326c60-4005-4e61-8c84-a71166c55a5e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Inspiring Mini...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FVAXItKojEQXmXUs4prJNIftWE6T2%2Fresized%2F1684086013193_500x500.webp?alt=media&token=20b664d5-8e4a-4aa0-813e-e85cb62653f4" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Mystical Golde...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FjEY0vtwRxod3rQYgO8RXcWQZD0Q2%2Fresized%2F1684183168165_500x500.webp?alt=media&token=e771bdea-967c-40fa-b199-6bbff9c186a7" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Graffiti Art Po..</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FAQiQ52YkpsRMq9I0qcZuil3gW1z1%2Fresized%2F1684186600407_500x500.webp?alt=media&token=089af98f-ec34-4737-9a50-d7980238691e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Quirky Cubis...</h6>
+            </div>
+          </div> 
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2Fxz9dllWexSP5z5zD72qgr1Ttszm2%2Fresized%2F1684177088670_500x500.webp?alt=media&token=83b9133d-0d96-46b2-aac8-12b57f81ef12" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Epic Fantasy s..</h6>
+            </div>
+          </div>          
+        </div>
+        <div className="prompts-container">
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FVAXItKojEQXmXUs4prJNIftWE6T2%2Fresized%2F1684174330042_500x500.webp?alt=media&token=83326c60-4005-4e61-8c84-a71166c55a5e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Inspiring Mini...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FVAXItKojEQXmXUs4prJNIftWE6T2%2Fresized%2F1684086013193_500x500.webp?alt=media&token=20b664d5-8e4a-4aa0-813e-e85cb62653f4" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Mystical Golde...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FjEY0vtwRxod3rQYgO8RXcWQZD0Q2%2Fresized%2F1684183168165_500x500.webp?alt=media&token=e771bdea-967c-40fa-b199-6bbff9c186a7" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Graffiti Art Po..</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FAQiQ52YkpsRMq9I0qcZuil3gW1z1%2Fresized%2F1684186600407_500x500.webp?alt=media&token=089af98f-ec34-4737-9a50-d7980238691e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Quirky Cubis...</h6>
+            </div>
+          </div> 
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2Fxz9dllWexSP5z5zD72qgr1Ttszm2%2Fresized%2F1684177088670_500x500.webp?alt=media&token=83b9133d-0d96-46b2-aac8-12b57f81ef12" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Epic Fantasy s..</h6>
+            </div>
+          </div>          
+        </div>
+        <div className="prompts-container">
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FVAXItKojEQXmXUs4prJNIftWE6T2%2Fresized%2F1684174330042_500x500.webp?alt=media&token=83326c60-4005-4e61-8c84-a71166c55a5e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Inspiring Mini...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FVAXItKojEQXmXUs4prJNIftWE6T2%2Fresized%2F1684086013193_500x500.webp?alt=media&token=20b664d5-8e4a-4aa0-813e-e85cb62653f4" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Mystical Golde...</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FjEY0vtwRxod3rQYgO8RXcWQZD0Q2%2Fresized%2F1684183168165_500x500.webp?alt=media&token=e771bdea-967c-40fa-b199-6bbff9c186a7" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Graffiti Art Po..</h6>
+            </div>
+          </div>
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2FAQiQ52YkpsRMq9I0qcZuil3gW1z1%2Fresized%2F1684186600407_500x500.webp?alt=media&token=089af98f-ec34-4737-9a50-d7980238691e" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Quirky Cubis...</h6>
+            </div>
+          </div> 
+          <div className="prompt">
+            <img src="https://firebasestorage.googleapis.com/v0/b/promptbase.appspot.com/o/DALLE_IMAGES%2Fxz9dllWexSP5z5zD72qgr1Ttszm2%2Fresized%2F1684177088670_500x500.webp?alt=media&token=83b9133d-0d96-46b2-aac8-12b57f81ef12" alt="prompt" />
+            <div className="ai-tag">
+              <span>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLCOeMuhq5A_kjK_pw-fVzOdsc_cVs46cktg&usqp=CAU" alt="AI" />
+                <h6>Midjourney</h6>
+              </span>
+            </div>
+            <div className="nameTag">
+              <h6>Epic Fantasy s..</h6>
+            </div>
+          </div>          
+        </div>
+        </div>
+        <a href="" className='profile-market' >Browse Marketplace</a>
+        </div>
+    </div>
+  )
+}
